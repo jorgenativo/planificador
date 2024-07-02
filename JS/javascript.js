d4b594622cbbe0584a242fd59d5430901c67244d
jorgenativo <jorgenativo2023@gmail.com>
2024-07-02 10:04:36 +0200

    cambiar colores de tareas

--- JS/javascript.js
+++ JS/javascript.js
@@ -2,3 +2,6 @@ function eliminar(){
  sessionStorage.setItem("");
  document.getElementById("tarea5").value="";
  }
+ const dias= ['Evaluar alcance del proyecto','Diseñar plantilla','Revisar y corregir','Testear funciones','Presentar proyecto'];
+ delete(dias)
+ console.log(dias)
