8351e636925c219fb2cbb025531e7a1e99df6ae7
jorgenativo <jorgenativo2023@gmail.com>
2024-07-08 17:37:54 +0200

    agregado comentario en JS

--- JS/javascript.js
+++ JS/javascript.js
@@ -3,6 +3,7 @@ function eliminar(){
  sessionStorage.setItem("");
  document.getElementById("tarea5").value="";
  }
+ //se agregó una constante que implica todas las actividades realizadas
  const dias= ['Evaluar alcance del proyecto','Diseñar plantilla','Revisar y corregir','Testear funciones','Presentar proyecto'];
  delete(dias)
  console.log(dias)
